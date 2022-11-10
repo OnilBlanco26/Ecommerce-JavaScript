1374445b4890c74a9e6d76e4c8804e4dc6cf0563
Andrés <andrescn1007@gmail.com>
2022-11-10 19:10:10 -0400

    test

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -67,17 +67,17 @@ const db = [
   },
   {
     id: 9,
-    name: 'Radeon 6600 XT MSI',
+    name: 'Radeon 6950 XT MSI',
     price: 586,
-    image: 'assets/img/radeon-6600xt.png',
+    image: 'assets/img/radeon-6950XT.png',
     category: 'GPUs',
     quantity: 4
   },
   {
     id: 10,
-    name: 'Radeon 6500 XT GIGABYTE',
+    name: 'Radeon 6900 XT Aorus',
     price: 586,
-    image: 'assets/img/radeon-6600xt.png',
+    image: 'assets/img/radeon-6900XT.png',
     category: 'GPUs',
     quantity: 4
   },
@@ -368,6 +368,7 @@ productContainer.addEventListener('click', function (e) {
     const id = +add.dataset.id
     addToCart(id)
   }
+  console.log('me estoy ejecutando')
 })
 
 cartContainer.addEventListener('click', function (e) {
